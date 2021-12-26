8f842032c066ddf20536d5586f2a8f5c1f88e77f
lordvlads77 <81182279+lordvlads77@users.noreply.github.com>
2021-12-26 04:31:00 -0600

    WebCrawler WIP | v0.1.1

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
 <?php
 
-$start = "http://localhost/se/test.html";
+$start = "http://localhost/";
+
+function follow_links($url)
+{
+  $doc = new DOMDocument();
+  $doc->loadHTML(file_get_contents($url));
+}
+
+follow_links($start);
